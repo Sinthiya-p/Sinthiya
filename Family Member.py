b425129c0b52002eaad99680813b9557e01a2ba9
Sinthiya-p <sinthiyaparamasivan@gmail.com>
2025-10-28 17:46:28 +0530

    Add files via upload

---
+++ Family Member.py
@@ -0,0 +1,10 @@
+Name="Sinthiya"
+Fathername="Paramasivan"
+Mothername="Arunachalakani"
+Brother="Sathiyakannan"
+Sister="Sathiyapriya"
+print("My Name is",Name)
+print("My Father Name",Fathername)
+print("My Mother Name",Mothername)
+print("My Brother Name",Brother)
+print("My Sister Name",Sister)
