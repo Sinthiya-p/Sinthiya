6ae9381aa84231fb1e3c811baf8f2c5d19ed8fd6
Sinthiya-p <sinthiyaparamasivan@gmail.com>
2025-10-28 18:02:25 +0530

    Add files via upload

---
+++ Sinthiya.py
@@ -0,0 +1,17 @@
+Name=Sinthiya
+Age=23
+Height=125
+Weight=33.5
+City=Tenkasi
+Student=False
+Mark=299
+Schoolname=AvvaiyarGovernmentgirlsher.sec.school
+print("My Name Is",Name)
+print("My Age",Age)
+print("My Height",Height)
+print("My Weight",Weight)
+print("My City",City)
+print("Student",Student)
+print("Mark",mark)
+print("School Name",SchoolName)
+
