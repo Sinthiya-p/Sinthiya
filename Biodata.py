c30babdd2944bb007e77b96044797b203e4a07c7
Sinthiya-p <sinthiyaparamasivan@gmail.com>
2025-10-28 17:11:40 +0530

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,20 @@
+name="Sinthiya"
+Age=23
+DOB=1472002
+Gender="Female"
+FatherName="Paramasivan"
+Address="3/231, Keela Street, Pethanadarpatti,Alangulam(Tk),Tenkasi(Dt),627808"
+Skill="Basic Computer"
+Hobbies="Dance"
+SingleChild="False"
+Weight=33.5
+print("My name is",name)
+print("My age is",Age)
+print("My Date of Birth",DOB)
+print("Gender",Gender)
+print("My Father Name is",FatherName)
+print("Address",Address)
+print("Skill",Skill)
+print("Hobbies",Hobbies)
+print("Single Child",SingleChild)
+print("My weight is",Weight)
