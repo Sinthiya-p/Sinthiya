b411b7eaa20662f4f00df796509509fd4d820a06
Sinthiya-p <sinthiyaparamasivan@gmail.com>
2025-11-03 17:16:21 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,29 @@
+print("print 1 to 20")
+for i in range(1,21):
+         print(i)
+print("print even number")         
+for i in range(2,51,2):
+         print(i)
+print("print odd number")         
+for i in range(1,51,3):
+         print(i)
+print("square numbers")
+for i in range(1,16):
+         print(i**2)
+print("cube of number")
+for i in range(1,11):
+         print(i**3)
+print("revers order numbers")
+for i in range(10,0,-1):
+         print(i)
+print("mutiplication table of 5")
+for i in range(1,11):
+         print(f"5 * {i} = {5*i}")
+print("string one by one")
+string="sintthiya"
+for char in string:
+    print(char)
+print("numbers divisible by 3 between 1 and 31")
+for i in range(1,31):
+    if(i%3==0):
+              print(i)
